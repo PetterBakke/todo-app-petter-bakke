433f4ad6d69412f1a3782d277b1de37083a272b6
PetterBakke <74598862+PetterBakke@users.noreply.github.com>
2023-01-11 18:09:10 +0100

    design edits

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./sass/style.scss";
+import Header from './components/header/Header';
+import TodoList from './components/todolist/TodoList';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
